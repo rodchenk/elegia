9fd1757b02ad13ff80cc1efcb8617edf57e86add
rodchenk <rodchenk@th-brandenburg.de>
2018-12-09 21:55:25 +0100

    edited script

--- import-db.bat
+++ import-db.bat
@@ -1,7 +1,7 @@
 @echo 'PASSWORD IST LEER, EINFACH ENTER CLICKEN'
 @echo off
 cd ../mysql/bin/
-@echo o
+@echo on
 mysql.exe -u root -p -e "CREATE DATABASE IF NOT EXISTS elegia"
 mysql.exe -u root -p elegia < ../../htdocs/db-scheme.sql
 @echo 'successfully completed, guys'
